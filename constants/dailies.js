76086fe3504e8f50f5870a9c4a707231dd475b99
jjperezaguinaga <me@jjperezaguinaga.com>
2020-11-13 17:04:43 +0000

    Adding daily 4

--- constants/dailies.js
+++ constants/dailies.js
@@ -1,11 +1,13 @@
 import { DAILY_1_RESULTS } from './daily/task_1';
 import { DAILY_2_RESULTS } from './daily/task_2';
 import { DAILY_3_RESULTS } from './daily/task_3';
+import { DAILY_4_RESULTS } from './daily/task_4';
 
 export const DAILIES_SCORE_ARRAY = [
     DAILY_1_RESULTS,
     DAILY_2_RESULTS,
     DAILY_3_RESULTS,
+    DAILY_4_RESULTS
 ]
 
 export const DAILIES_SCORE_ARRAY_MAP = DAILIES_SCORE_ARRAY.map((daily, index) => ({
