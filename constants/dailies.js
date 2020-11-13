e9de574427b24975a0cbe86b94b7ef431233e7ab
jjperezaguinaga <me@jjperezaguinaga.com>
2020-11-13 06:06:39 +0000

    Added day 3 and a breakdown per score

--- constants/dailies.js
+++ constants/dailies.js
@@ -1,7 +1,15 @@
 import { DAILY_1_RESULTS } from './daily/task_1';
 import { DAILY_2_RESULTS } from './daily/task_2';
+import { DAILY_3_RESULTS } from './daily/task_3';
 
 export const DAILIES_SCORE_ARRAY = [
     DAILY_1_RESULTS,
     DAILY_2_RESULTS,
+    DAILY_3_RESULTS,
 ]
+
+export const DAILIES_SCORE_ARRAY_MAP = DAILIES_SCORE_ARRAY.map((daily, index) => ({
+    name: `Daily ${index + 1}`,
+    maxPoints: Math.max(...Object.values(daily)),
+    daily
+}))
