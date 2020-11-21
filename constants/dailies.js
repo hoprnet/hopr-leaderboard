ffbb3570cdd0ffcc35a25dc71c55616a1d6138d7
kitschbent <34662130+kitschbent@users.noreply.github.com>
2020-11-21 17:56:31 -0500

    Update dailies.js

--- constants/dailies.js
+++ constants/dailies.js
@@ -4,6 +4,12 @@ import { DAILY_3_RESULTS } from './daily/task_3';
 import { DAILY_4_RESULTS } from './daily/task_4';
 import { DAILY_5_RESULTS } from './daily/task_5';
 import { DAILY_6_RESULTS } from './daily/task_6';
+import { DAILY_7_RESULTS } from './daily/task_7';
+import { DAILY_8_RESULTS } from './daily/task_8';
+import { DAILY_9_RESULTS } from './daily/task_9';
+import { DAILY_10_RESULTS } from './daily/task_10';
+import { DAILY_11_RESULTS } from './daily/task_11';
+import { DAILY_11W_RESULTS } from './daily/task_11w';
 
 export const DAILIES_SCORE_ARRAY = [
     DAILY_1_RESULTS,
@@ -11,7 +17,13 @@ export const DAILIES_SCORE_ARRAY = [
     DAILY_3_RESULTS,
     DAILY_4_RESULTS,
     DAILY_5_RESULTS,
-    DAILY_6_RESULTS
+    DAILY_6_RESULTS,
+    DAILY_7_RESULTS,
+    DAILY_8_RESULTS,
+    DAILY_9_RESULTS,
+    DAILY_10_RESULTS,
+    DAILY_11_RESULTS,
+    DAILY_11W_RESULTS
 ]
 
 export const DAILIES_SCORE_ARRAY_MAP = DAILIES_SCORE_ARRAY.map((daily, index) => ({
