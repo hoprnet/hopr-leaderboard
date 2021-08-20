a00e25b734b62e49371e4246eee4769c607b149d
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-20 19:20:52 +0000

    feat: Adding funding functionality for mHOPR tokens

--- pages/api/faucet/fund/[address].js
+++ pages/api/faucet/fund/[address].js
@@ -1,18 +1,22 @@
 import {
   HOPR_WEB3_SIGNATURE_TYPES,
   HOPR_WEB3_SIGNATURE_DOMAIN,
+  TOKEN_ADDRESS_POLYGON,
 } from "../../../../constants/hopr";
-import { verifyTypedData } from "ethers/lib/utils";
+import stakers from "../../../../constants/stake/sixth_august.json";
+import devs from "../../../../constants/stake/dev_wallets.json";
+import HOPR_TOKEN_ABI from "../../../../constants/HoprTokenABI";
+import { providers, utils, Wallet, Contract } from 'ethers'
 
 //@TODO:
 //1. [✅] Get address and signature, verify that indeed they match.
-//2. [ ] Using that signature, look up the eligibility for funds.
-//3. [ ] Trigger a transferFrom call with the eligible funds and send receipt.
+//2. [✅] Using that signature, look up the eligibility for funds.
+//3. [⏰] Trigger a transferFrom call with the eligible funds and send receipt.
 export default async (req, res) => {
   const { address } = req.query;
   const { signature, message } = req.body;
 
-  const signerAddress = verifyTypedData(
+  const signerAddress = utils.verifyTypedData(
     HOPR_WEB3_SIGNATURE_DOMAIN,
     HOPR_WEB3_SIGNATURE_TYPES,
     message,
@@ -20,10 +24,45 @@ export default async (req, res) => {
   );
   const isValidSignature = address == signerAddress;
 
-  return res.json({
-    status: "ok",
-    address,
-    signature,
-    isValidSignature,
-  });
+  if (isValidSignature) {
+    const checksumedAddress = utils.getAddress(address);
+    const eligible = [stakers, devs]
+      .map((collection) =>
+        collection.find((staker) => staker.account === checksumedAddress)
+      )
+      .reduce((acc, val) => (acc ? acc : val));
+
+    const provider = new providers.JsonRpcProvider(
+      `https://polygon-mainnet.infura.io/v3/${process.env.HOPR_DASHBOARD_API_INFURA_ID}`
+    );
+    const wallet = new Wallet(
+      process.env.HOPR_DASHBOARD_API_PRIVATE_KEY,
+      provider
+    );
+    const hoprTokenContract = new Contract(
+      TOKEN_ADDRESS_POLYGON,
+      HOPR_TOKEN_ABI,
+      wallet
+    );
+
+    let transferAmount
+    if (eligible) {
+      // Send 0.01 MATIC, and eligible.actual_stake mHOPR to message.ethAddress
+      transferAmount = '3';
+    } else {
+      // Send 0 MATIC, and 10 mHOPR to message.ethAddress
+      transferAmount = '1';  
+    }
+    const tx = await hoprTokenContract.transfer(message.ethAddress, utils.parseEther(transferAmount));
+    return res.status(200).json({
+      status: "ok",
+      address,
+      tx,
+    });
+  } else {
+    return res.json({
+      status: "err",
+      message: "Signature is invalid.",
+    });
+  }
 };
