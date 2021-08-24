ac247eeb6c9ed724516b4b01c4082362aa0b43ff
jjperezaguinaga <me@jjperezaguinaga.com>
2021-08-24 09:31:56 +0000

    chore: Better org and added CM leads

--- constants/stake/index.js
+++ constants/stake/index.js
@@ -1,5 +1,6 @@
-import stakers from "./sixth_august.json";
-import devs from "./dev_wallets.json";
+import stakers from "./stakers.json";
+import devs from "./devs.json";
 import ambassadors from "./ambassadors.json";
+import community_leads from "./community_leads.json";
 
-export const whitelisted = [stakers, devs, ambassadors];
+export const whitelisted = [stakers, devs, ambassadors, community_leads];
