24f8860c99b6230d5f050a11c7e21643b20bd70c
jjperezaguinaga <me@jjperezaguinaga.com>
2021-09-03 14:57:26 +0000

    feat: Adding writing and retrieval endpoint

--- pages/api/sign/verify/[address].js
+++ pages/api/sign/verify/[address].js
@@ -1,8 +1,17 @@
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import KeyResolver from 'key-did-resolver'
+import { DID } from 'dids'
+import CeramicClient from '@ceramicnetwork/http-client'
+import { TileDocument } from '@ceramicnetwork/stream-tile'
 import {
   HOPR_WEB3_SIGNATURE_DOMAIN,
   HOPR_WEB3_SIGNATURE_FOR_NODE_TYPES,
   TOKEN_ADDRESS_POLYGON,
 } from "../../../../constants/hopr";
+import {
+  CERAMIC_API_URL
+} from "../../../../constants/ceramic";
+
 import { verifySignatureFromPeerId } from "@hoprnet/hopr-utils";
 import { formatEther } from "@ethersproject/units";
 import HOPR_TOKEN_ABI from "../../../../constants/HoprTokenABI";
@@ -14,6 +23,12 @@ import { providers, utils, Wallet, Contract } from "ethers";
 // to get a valid signature.
 // see https://github.com/hoprnet/hoprnet/blob/master/packages/core/src/index.ts#L865-L870
 const HOPR_PREFIX = "HOPR Signed Message: ";
+
+const secretKey = Uint8Array.from(utils.arrayify(`0x${process.env.HOPR_DASHBOARD_API_PRIVATE_KEY}`))
+const provider = new Ed25519Provider(secretKey)
+const did = new DID({ provider, resolver: KeyResolver.getResolver() })
+const client = new CeramicClient(CERAMIC_API_URL)
+let tile = null
 
 export default async (req, res) => {
   const { address } = req.query;
@@ -39,10 +54,23 @@ export default async (req, res) => {
     );
 
     if (isAddressOwnerOfNode) {
+      // @TODO: Move from API endpoint as this gets called everytime.
+      if (!tile) {
+        await did.authenticate()
+        client.setDID(did)
+        tile = await TileDocument.create(client, {})
+      }
+
+      const records = await TileDocument.load(client, tile.id)
+      const mutatedRecords = Object.assign({}, records.content, { [hoprAddress]: ethAddress })
+      await tile.update(mutatedRecords)
+
       return res.status(200).json({
         status: "ok",
-        message: `Your signature match and we connected it to our database.`,
+        tile: tile.id.toString(),
+        message: `Your node was recorded into the Ceramic network.`,
       });
+
     } else {
       return res.status(200).json({
         status: "invalid",
